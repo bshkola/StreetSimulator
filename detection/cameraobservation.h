6545c4adf30eb2175aa093a76a09abb9068878cc
Wojciech Ceret <pscorek@gmail.com>
2014-12-16 02:59:32 +0100

    Added constructor to class CameraObservation.

--- detection/cameraobservation.h
+++ detection/cameraobservation.h
@@ -5,9 +5,13 @@
 #define CAMERAOBSERVATION_H
 
 #include "../common/camera.h"
-#include "../common/traffic/trafficparticipant.h"
 
 struct CameraObservation {
+
+    CameraObservation(Camera camera, float object_x, float object_y, float object_distance, float object_azimuth):
+        camera(camera), object_x(object_x), object_y(object_y), object_distance(object_distance), object_azimuth(object_azimuth)
+        {};
+
     Camera camera;
     float object_x, object_y, object_distance, object_azimuth;
 };
