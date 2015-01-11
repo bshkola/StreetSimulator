336bde2333eb13dcbc039943d91b6d568c833941
Wojciech Ceret <pscorek@gmail.com>
2015-01-11 18:17:59 +0100

    Time operations - first atempt.

--- model/engine/engine.cpp
+++ model/engine/engine.cpp
@@ -4,20 +4,43 @@ Engine::Engine(const ObjectsOnMap &objectsOnMap, ICameraNoise *iCameraNoise): ob
 {
     iCameraDetection = new CameraDetectionImpl(iCameraNoise);
     //this -> objectsOnMap = objectsOnMap;
-}
-
-void Engine::run()
-{
-    objectsPositionUpdater.update();
-    iCameraDetection -> calculate(objectsOnMap.getCameras(),
-                               objectsPositionUpdater.getTrafficParticipants(),
-                               database.getCameraObservation());
-    database.saveMeasurent();
-    //TODO repaint()
+    
+    finish_ = false;
 }
 
 Engine::~Engine()
 {
+    delete iCameraDetection;
+}
 
+void Engine::run()
+{
+    while(!finish_)
+    {
+        //TODO startTime = getTime();
+        startTime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now().time_since_epoch());
+
+        objectsPositionUpdater.update();
+        iCameraDetection -> calculate( objectsOnMap.getCameras(),
+                                       objectsPositionUpdater.getTrafficParticipants(),
+                                       database.getCameraObservation());
+        database.saveMeasurent();
+        //TODO repaint()
+        
+        //TODO wait(getTime() - startTime);
+        endTime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now().time_since_epoch());
+        if(endTime - startTime < millisecondsInQuantum)
+        {
+            //TODO wait(getTime() - startTime);
+            std::this_thread::sleep_for(std::chrono::milliseconds(millisecondsInQuantum - (endTime - startTime)));
+        }
+        
+    }
+    std::cout << "Simulation stopped" << std::endl;
+}
+
+void Engine::finish()
+{
+    finish_ = true;
 }
 
--- model/engine/engine.h
+++ model/engine/engine.h
@@ -6,11 +6,16 @@
 #include "../objectsonmap.h"
 #include "../../detection/cameranoiseimpl.h"
 #include "../database/database.h"
+#include <chrono>
+#include <thread>
+
 class Engine
 {
 public:
     Engine(const ObjectsOnMap &ObjectsOnMap, ICameraNoise *iCameraNoise);
     ~Engine();
+    
+    void finish();//set stop flag
 
     void run(); //running engine
 private:
@@ -19,6 +24,9 @@ private:
     const ObjectsOnMap &objectsOnMap; //cameras on the map
     Database database; //database
     //TODO referencja na widok
+    
+    bool finish_; //if true then stop calculations
+    std::chrono::milliseconds startTime, endTime;
 };
 
 #endif // ENGINE_H
